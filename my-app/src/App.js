aaea05c0a3da5db5243f3d7b020c4757e51b6b30
zevi zeltzer <zz0583294131@gmail.com>
2024-07-29 17:07:33 +0300

    add a prompt

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,10 +4,13 @@ import ListItems from './ListItems';
 import Time from './Time';
 import ButAlert from './ButAlert';
 
+const titleName = prompt("Please enter a title name!!");
+
 function App() {
+  
   return (
     <div className="App">
-      <h1>Personal diary</h1>
+      <h1>{titleName}</h1>
       <ListItems/>
       <Time/>
       <ButAlert/>
